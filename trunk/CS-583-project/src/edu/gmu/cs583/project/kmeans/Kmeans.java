17dc5a5c33dc2ed2f80249e26ea09aa1d926cf18
chris.j.andrade <chris.j.andrade@acb9212a-a902-11dd-91bf-632da8b6e10b>
2008-11-02 18:33:38 +0000

    11/2/2008 - chris.j.andrade@gmail.com

---
+++ trunk/CS-583-project/src/edu/gmu/cs583/project/kmeans/Kmeans.java
@@ -0,0 +1,13 @@
+package edu.gmu.cs583.project.kmeans;
+
+public class Kmeans {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
